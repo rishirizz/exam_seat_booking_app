c82fb006aee3508f8c3829048bd746459a4a9ab1
rishirizz <arijeetc6@gmail.com>
2022-02-24 22:25:17 +0530

    data rendered from the response

--- lib/screens/user_detail_screen.dart
+++ lib/screens/user_detail_screen.dart
@@ -1,4 +1,7 @@
+import 'package:exam_seat_booking_app/services/api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class UserRegistrationScreen extends StatefulWidget {
   const UserRegistrationScreen({Key? key}) : super(key: key);
@@ -8,12 +11,87 @@ class UserRegistrationScreen extends StatefulWidget {
 }
 
 class _UserRegistrationScreenState extends State<UserRegistrationScreen> {
+  Map examDetails = {};
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    print('object');
+    getNames().then((response) {
+      setState(() {
+        examDetails = response;
+        print('ExamDetails=====>$examDetails');
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.all(10.0),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Card(
+                  elevation: 4,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          child: FadeInImage.memoryNetwork(
+                            imageErrorBuilder: (BuildContext context,
+                                    Object exception, StackTrace? stackTrace) =>
+                                Image.asset('images/inventory_image.png'),
+                            fit: BoxFit.cover,
+                            // width: 70,
+
+                            placeholder: kTransparentImage,
+                            image: examDetails['image'],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(
+                        flex: 7,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Text(
+                                examDetails['title'],
+                              ),
+                              Divider(
+                                color: Colors.black,
+                              ),
+                              Text(
+                                examDetails['year'],
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                examDetails['detail1'],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 Future getNames() async {
   Uri url = Uri.parse(
       'https://3a178515-5a1f-4da4-b47b-b9e825f92625.mock.pstmn.io/getExam/');
   final response = await http.get(url);
+  // print(response.body);
   if (response.statusCode == 200) {
     return json.decode(response.body);
   } else {
